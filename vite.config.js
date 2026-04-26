7a76075ba265aa59d15d1dbef4b3478ee3a53738
Aman Dahayat <89609354+Aman24-0@users.noreply.github.com>
2026-04-27 00:20:57 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: false, // Hum public folder wala manifest use karenge
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/image\.tmdb\.org\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'tmdb-images',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      }
+    })
+  ],
+  build: {
+    target: 'esnext',
+  },
+});
