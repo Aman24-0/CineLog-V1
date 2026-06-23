c9260fa75244f16299f149c17044de129753a1a2
Aman Dahayat <89609354+Aman24-0@users.noreply.github.com>
2026-06-24 01:36:01 +0530

    Refactor DirectPlayPlayer to use video.js

--- src/components/DirectPlayPlayer.jsx
+++ src/components/DirectPlayPlayer.jsx
@@ -1,82 +1,62 @@
-import { onMount, onCleanup, Show } from "solid-js";
+import { onMount, onCleanup } from "solid-js";
 
-import "vidstack/player/styles/default/theme.css";
-import "vidstack/player/styles/default/layouts/video.css";
-
-import "vidstack/player";
-import "vidstack/player/layouts/default";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
 
 export function DirectPlayPlayer(props) {
-  let playerRef;
+  let videoRef;
+  let player;
 
   onMount(() => {
-    if (!playerRef) return;
+    player = videojs(videoRef, {
+      controls: true,
+      autoplay: true,
+      responsive: true,
+      fluid: true,
+      preload: "auto",
+      playbackRates: [0.5, 1, 1.25, 1.5, 2],
+      controlBar: {
+        pictureInPictureToggle: true,
+      },
+      sources: [
+        {
+          src: props.src,
+          type: "video/mp4",
+        },
+      ],
+    });
 
-    const handleTimeUpdate = () => {
+    player.ready(() => {
+      if (props.startTime > 0) {
+        player.currentTime(props.startTime);
+      }
+    });
+
+    player.on("timeupdate", () => {
       if (!props.onProgress) return;
 
       props.onProgress({
-        currentTime: playerRef.currentTime || 0,
-        duration: playerRef.duration || 0,
+        currentTime: player.currentTime(),
+        duration: player.duration(),
       });
-    };
-
-    const handleLoadedMetadata = () => {
-      if (props.startTime > 0) {
-        playerRef.currentTime = props.startTime;
-      }
-    };
-
-    playerRef.addEventListener(
-      "time-update",
-      handleTimeUpdate
-    );
-
-    playerRef.addEventListener(
-      "loaded-metadata",
-      handleLoadedMetadata
-    );
-
-    onCleanup(() => {
-      playerRef.removeEventListener(
-        "time-update",
-        handleTimeUpdate
-      );
-
-      playerRef.removeEventListener(
-        "loaded-metadata",
-        handleLoadedMetadata
-      );
     });
+  });
+
+  onCleanup(() => {
+    if (player) {
+      player.dispose();
+    }
   });
 
   return (
     <div class="w-full h-full bg-black">
-      <media-player
-        ref={playerRef}
-        title={props.title || "Video"}
-        playsinline
-        autoplay
-        class="w-full h-full"
-      >
-        <media-provider>
-          {/* MP4 Source */}
-          <source
-            src={props.src}
-            type="video/mp4"
-          />
-
-          <Show when={props.poster}>
-            <media-poster
-              src={props.poster}
-              class="vds-poster"
-            />
-          </Show>
-        </media-provider>
-
-        {/* Full Vidstack UI */}
-        <media-video-layout />
-      </media-player>
+      <div data-vjs-player>
+        <video
+          ref={videoRef}
+          class="video-js vjs-big-play-centered"
+          playsinline
+        />
+      </div>
     </div>
   );
 }
